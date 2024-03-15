430ae3e5384218107edfee01b3baec1f15b73a4c
Thomas <thurtig.email@gmail.com>
2024-03-15 12:56:41 +0100

    add better defaults

--- src/config.ts
+++ src/config.ts
@@ -30,8 +30,15 @@ export const config = {
         isDefault: true,
       },
       {
+        id: "h265",
+        label: "MP4 (H.265)",
+        value: "MP4 (H.265)",
+        ext: "mp4",
+        mimetype: "video/mp4",
+      },
+      {
         id: "webm",
-        label: "WEBM",
+        label: "WEBM (VP8)",
         value: "WEBM",
         ext: "webm",
         mimetype: "video/webm",
@@ -57,16 +64,8 @@ export const config = {
       {
         id: "mp4-vcodec",
         label: "Video Codec",
-        value: "vcodec",
-        options: [
-          {
-            id: "mp4-vcodec-h264",
-            label: "H.264",
-            value: "-vcodec libx264",
-            isDefault: true,
-          },
-          { id: "mp4-vcodec-h265", label: "H.265", value: "-vcodec libx265" },
-        ],
+        value: "-vcodec libx264",
+        isDefault: true,
       },
       {
         id: "mp4-quality",
@@ -137,19 +136,96 @@ export const config = {
         ],
       },
     ],
+    h265: [
+      {
+        id: "h265-vcodec",
+        label: "Video Codec",
+        value: "-vcodec libx265",
+        isDefault: true,
+      },
+      {
+        id: "h265-quality",
+        label: "Quality",
+        value: "quality",
+        options: [
+          {
+            id: "265-quality-low",
+            label: "Low",
+            value: "-crf 30",
+          },
+          {
+            id: "265-quality-medium",
+            label: "Medium",
+            value: "-crf 25",
+            isDefault: true,
+          },
+          {
+            id: "265-quality-high",
+            label: "High",
+            value: "-crf 20",
+          },
+          {
+            id: "265-quality-lossless",
+            label: "Lossless",
+            value: "-crf 0",
+          },
+        ],
+      },
+      {
+        id: "h265-acodec",
+        label: "Audio Codec",
+        value: "acodec",
+        options: [
+          {
+            id: "h265-acodec-no-audio",
+            label: "remove audio",
+            value: "-an",
+          },
+          { id: "h265-acodec-aac", label: "AAC", value: "-acodec aac" },
+          {
+            id: "h265-acodec-mp3",
+            label: "MP3",
+            value: "-acodec libmp3lame",
+            isDefault: true,
+          },
+        ],
+      },
+      {
+        id: "h265-resolution",
+        label: "Resolution",
+        value: "resolution",
+        options: [
+          {
+            id: "h265-source",
+            label: "as source",
+            value: "",
+            isDefault: true,
+          },
+          {
+            id: "h265-480p",
+            label: "480p (SD)",
+            value: "-vf scale=854:-2",
+          },
+          {
+            id: "h265-720p",
+            label: "720p (HD)",
+            value: "-vf scale=1280:-2",
+          },
+          {
+            id: "h265-1080p",
+            label: "1080p (Full HD)",
+            value: "-vf scale=1920:-2",
+          },
+          { id: "h265-2k", label: "2K", value: "-vf scale=2048:-2" },
+        ],
+      },
+    ],
     webm: [
       {
         id: "webm-vcodec",
         label: "Video Codec",
-        value: "vcodec",
-        options: [
-          {
-            id: "webm-vcodec-vp8",
-            label: "VP8",
-            value: "-vcodec libvpx",
-            isDefault: true,
-          },
-        ],
+        value: "-vcodec libvpx",
+        isDefault: true,
       },
       {
         id: "webm-quality",
@@ -157,25 +233,30 @@ export const config = {
         value: "quality",
         options: [
           {
+            id: "webm-quality-lower",
+            label: "Loooow",
+            value: "-crf 10 -b:v 1M",
+          },
+          {
             id: "webm-quality-low",
             label: "Low",
-            value: "-crf 30",
+            value: "-crf 10 -b:v 2M",
           },
           {
             id: "webm-quality-medium",
             label: "Medium",
-            value: "-crf 20",
+            value: "-crf 8 -b:v 5M",
             isDefault: true,
           },
           {
             id: "webm-quality-high",
             label: "High",
-            value: "-crf 10",
+            value: "-crf 4 -b:v 10M",
           },
           {
             id: "webm-quality-lossless",
-            label: "Lossless",
-            value: "-crf 4",
+            label: "Ultra",
+            value: "-crf 4 -b:v 20M",
           },
         ],
       },
@@ -224,15 +305,8 @@ export const config = {
       {
         id: "ogv-vcodec",
         label: "Video Codec",
-        value: "vcodec",
-        options: [
-          {
-            id: "ogv-vcodec-theora",
-            label: "Theora",
-            value: "-vcodec libtheora",
-            isDefault: true,
-          },
-        ],
+        value: "-vcodec libtheora",
+        isDefault: true,
       },
       {
         id: "ogv-quality",
@@ -242,18 +316,23 @@ export const config = {
           {
             id: "ogv-quality-low",
             label: "Low",
-            value: "-q:v 7",
+            value: "-q:v 3",
           },
           {
             id: "ogv-quality-medium",
             label: "Medium",
-            value: "-q:v 5",
+            value: "-q:v 6",
             isDefault: true,
           },
           {
             id: "ogv-quality-high",
             label: "High",
-            value: "-q:v 3",
+            value: "-q:v 8",
+          },
+          {
+            id: "ogv-quality-highest",
+            label: "Highest",
+            value: "-q:v 10",
           },
         ],
       },
@@ -299,15 +378,22 @@ export const config = {
     ],
     gif: [
       {
+        id: "filter_complex",
+        label: "Palette",
+        value:
+          "-filter_complex loop={gif-loop},fps={gif-fps},scale={gif-scale}[s];[s]split[a][b];[a]palettegen[palette];[b][palette]paletteuse",
+        isDefault: true,
+      },
+      {
         id: "gif-fps",
         label: "Frames/sec",
         value: "fps",
         options: [
-          { id: "gif-fps-10", label: "10", value: "-r 10", isDefault: true },
-          { id: "gif-fps-15", label: "15", value: "-r 15" },
-          { id: "gif-fps-20", label: "20", value: "-r 20" },
-          { id: "gif-fps-24", label: "24", value: "-r 24" },
-          { id: "gif-fps-30", label: "30", value: "-r 30" },
+          { id: "gif-fps-10", label: "10", value: "10", isDefault: true },
+          { id: "gif-fps-15", label: "15", value: "15" },
+          { id: "gif-fps-20", label: "20", value: "20" },
+          { id: "gif-fps-24", label: "24", value: "24" },
+          { id: "gif-fps-30", label: "30", value: "30" },
         ],
       },
       {
@@ -316,21 +402,21 @@ export const config = {
         value: "scale",
         options: [
           { id: "gif-scale-source", label: "source", value: "" },
-          { id: "gif-scale-80", label: "80px", value: "-vf scale=80:-1" },
+          { id: "gif-scale-80", label: "80px", value: "80:-1" },
           {
             id: "gif-scale-160",
             label: "160px",
-            value: "-vf scale=160:-1",
+            value: "160:-1",
           },
           {
             id: "gif-scale-320",
             label: "320px",
-            value: "-vf scale=320:-1",
+            value: "320:-1",
             isDefault: true,
           },
-          { id: "gif-scale-480", label: "480px", value: "-vf scale=480:-1" },
-          { id: "gif-scale-640", label: "640px", value: "-vf scale=640:-1" },
-          { id: "gif-scale-800", label: "800px", value: "-vf scale=800:-1" },
+          { id: "gif-scale-480", label: "480px", value: "480:-1" },
+          { id: "gif-scale-640", label: "640px", value: "640:-1" },
+          { id: "gif-scale-800", label: "800px", value: "800:-1" },
         ],
       },
       {
@@ -338,12 +424,12 @@ export const config = {
         label: "Loop",
         value: "loop",
         options: [
-          { id: "gif-loop-0", label: "no loop", value: "-loop -1" },
-          { id: "gif-loop-2", label: "one loop", value: "-loop 1" },
+          { id: "gif-loop-0", label: "no loop", value: "-1" },
+          { id: "gif-loop-2", label: "one loop", value: "1" },
           {
             id: "gif-loop-1",
             label: "infinite",
-            value: "-loop 0",
+            value: "0",
             isDefault: true,
           },
         ],
